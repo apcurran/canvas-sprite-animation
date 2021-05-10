2bead2aac2425ad7f41d132d8b495a680241f3da
Alex Curran <alexcurran.va@gmail.com>
2021-05-10 13:38:20 -0500

    Basix setup

--- sprite-animation.js
+++ sprite-animation.js
@@ -8,79 +8,27 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
-/**
- * Dog sprite module
- * @typedef {object} dog
- * @property {HTMLImageElement} dogImg
- * 
- * @return {dog}
- */
-const dogSprite = (() => {
-    const dogImg = new Image();
-    dogImg.src = "./images/shadow-dog.png";
+const playerImg = new Image();
+playerImg.src = './images/shadow-dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
-    const spriteWidth = 575;
-    const spriteHeight = 523;
-    const staggerFrames = 5;
-
-    let frameX = 0;
-    let frameY = 0;
-    let gameFrame = 0;
-
-    function getFrameX() {
-        return frameX;
-    }
-
-    function setFrameX(amt) {
-        frameX += amt;
-    }
-
-    function resetFrameX() {
-        frameX = 0;
-    }
-
-    function getFrameY() {
-        return frameY;
-    }
-
-    function getGameFrame() {
-        return gameFrame;
-    }
-
-    function incrementGameFrame() {
-        gameFrame++;
-    }
-
-    return {
-        dogImg,
-        spriteWidth,
-        spriteHeight,
-        getFrameX,
-        setFrameX,
-        resetFrameX,
-        getFrameY,
-        getGameFrame,
-        incrementGameFrame,
-        staggerFrames,
-    };
-})();
-
-/**
- * Animation loop
- */
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(dogSprite.dogImg, dogSprite.getFrameX() * dogSprite.spriteWidth, dogSprite.getFrameY() * dogSprite.spriteHeight, dogSprite.spriteWidth, dogSprite.spriteHeight, 0, 0, dogSprite.spriteWidth, dogSprite.spriteHeight);
+    ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
-    if (dogSprite.getGameFrame() % dogSprite.staggerFrames === 0) {
-        if (dogSprite.getFrameX() < 6) {
-            dogSprite.setFrameX(1);
+    if (gameFrame % staggerFrames === 0) {
+        if (frameX < 6) {
+            frameX++;
         } else {
-            dogSprite.resetFrameX();
+            frameX = 0;
         }
     }
-
-    dogSprite.incrementGameFrame();
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 
