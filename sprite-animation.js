9429f11a29d3c91dce2af81a491d1e7686da03a7
Alex Curran <alexcurran.va@gmail.com>
2021-05-10 14:34:17 -0500

    Finish animations with drop down menu for selection

--- sprite-animation.js
+++ sprite-animation.js
@@ -1,5 +1,12 @@
 "use strict";
 
+let playerState = "sit";
+const dropDown = document.getElementById("animations");
+
+dropDown.addEventListener("change", (event) => {
+    playerState = event.target.value;
+})
+
 /** @type {HTMLCanvasElement} */
 // @ts-ignore
 const canvas = document.getElementById("canvas1");
@@ -12,22 +19,77 @@ const playerImg = new Image();
 playerImg.src = './images/shadow-dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
 const staggerFrames = 5;
+let spriteAnimations = [];
+let animationStates = [
+    {
+        name: "idle",
+        frames: 7
+    },
+    {
+        name: "jump",
+        frames: 7
+    },
+    {
+        name: "fall",
+        frames: 7
+    },
+    {
+        name: "run",
+        frames: 9
+    },
+    {
+        name: "dizzy",
+        frames: 11
+    },
+    {
+        name: "sit",
+        frames: 5
+    },
+    {
+        name: "roll",
+        frames: 7
+    },
+    {
+        name: "bite",
+        frames: 7
+    },
+    {
+        name: "ko",
+        frames: 12
+    },
+    {
+        name: "getHit",
+        frames: 4
+    },
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        location: []
+    };
+
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+
+        frames.location.push({ x: positionX, y: positionY });
+    }
+
+    spriteAnimations[state.name] = frames;
+});
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
-    if (gameFrame % staggerFrames === 0) {
-        if (frameX < 6) {
-            frameX++;
-        } else {
-            frameX = 0;
-        }
-    }
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].location.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].location[position].y;
+
+    ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
     gameFrame++;
     requestAnimationFrame(animate);
 }
